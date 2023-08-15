44382c96e88a4fa88367f5ec379030f6fe6f22af
TurovaE <ekaterina.turova@mail.ru>
2023-08-15 22:05:00 +0700

    fix basename problem

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./css/index.css";
 import { BrowserRouter } from "react-router-dom";
+const BASENAME = import.meta.env.VITE_PUBLIC_BASENAME || "/";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={BASENAME}>
     <App />
   </BrowserRouter>
 );
